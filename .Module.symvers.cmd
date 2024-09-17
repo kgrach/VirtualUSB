a10c0313526f15536057fc804adc47ac8cfb4f65
Kirill Grachev <pllayer@mail.ru>
2024-09-17 17:06:38 +0700

    Clean prev commit

--- .Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/gka/repos/VirtualUSB/Module.symvers :=  scripts/mod/modpost -M -m -a      -o /home/gka/repos/VirtualUSB/Module.symvers -T /home/gka/repos/VirtualUSB/modules.order -i Module.symvers -e 
